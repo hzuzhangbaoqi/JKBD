1c0bc63f30b1a98c2ef5917397c54ca14664605b
zhangbaoqi <764522533@qq.com>
2017-06-27 21:59:15 +0800

    实现闪屏功能

---
+++ app/src/main/java/com/example/hzu/jkbd/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.example.hzu.jkbd;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/6/27.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    protected void onCreate(@Nullable Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_splash);
+        mcountDownTimer.start();
+
+    }
+
+    CountDownTimer mcountDownTimer = new CountDownTimer(2000, 1000) {
+        @Override
+        public void onTick(long millisUntilFinished) {
+
+        }
+
+        @Override
+        public void onFinish() {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+
+
+    };
+}
