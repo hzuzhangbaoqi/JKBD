d7aa09e250133890ee15ee97be9bba0ec6593565
zhangbaoqi <764522533@qq.com>
2017-06-28 23:54:50 +0800

    生成Question实体类

---
+++ app/src/main/java/com/example/hzu/jkbd/bean/Question.java
@@ -0,0 +1,102 @@
+package com.example.hzu.jkbd.bean;
+
+/**
+ * Created by Administrator on 2017/6/28.
+ */
+
+public class Question {
+
+    /**
+     * id : 10
+     * question : 这个标志是何含义？
+     * answer : 2
+     * item1 : 分向行驶车道
+     * item2 : 掉头和左转合用车道
+     * item3 : 禁止左转和掉头车道
+     * item4 : 直行和左转合用车道
+     * explains : 左转和掉头合并在一个标志里，你应该能看到的。
+     * url : http://images.juheapi.com/jztk/c1c2subject1/10.jpg
+     */
+
+    private int id;
+    private String question;
+    private String answer;
+    private String item1;
+    private String item2;
+    private String item3;
+    private String item4;
+    private String explains;
+    private String url;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getItem1() {
+        return item1;
+    }
+
+    public void setItem1(String item1) {
+        this.item1 = item1;
+    }
+
+    public String getItem2() {
+        return item2;
+    }
+
+    public void setItem2(String item2) {
+        this.item2 = item2;
+    }
+
+    public String getItem3() {
+        return item3;
+    }
+
+    public void setItem3(String item3) {
+        this.item3 = item3;
+    }
+
+    public String getItem4() {
+        return item4;
+    }
+
+    public void setItem4(String item4) {
+        this.item4 = item4;
+    }
+
+    public String getExplains() {
+        return explains;
+    }
+
+    public void setExplains(String explains) {
+        this.explains = explains;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
