9be015e13c820dafb8e18e61d6a6f4c9db8e4ab5
hzuzhangbaoqi <764522533@qq.com>
2017-06-29 16:57:22 +0800

    实现远端获取数据

--- app/src/main/java/com/example/hzu/jkbd/MainActivity.java
+++ app/src/main/java/com/example/hzu/jkbd/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.hzu.jkbd;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.example.hzu.jkbd.bean.ExamInformation;
+import com.example.hzu.jkbd.utils.OkHttpUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void test(View view) {
+        OkHttpUtils<ExamInformation> utils =new OkHttpUtils<>(getApplicationContext());
+        String url="http://101.251.196.90:8080/JztkServer/examInfo";
+        utils.url(url)
+                .targetClass(ExamInformation.class)
+                .execute(new OkHttpUtils.OnCompleteListener<ExamInformation>(){
+                    public void onSuccess(ExamInformation result){
+                        Log.e("main","result="+result);
+                    }
+                    public void onError(String error){
+                        Log.e("main","error="+error);
+                    }
+                });
+
+       startActivity(new Intent(MainActivity.this,ExamActivity.class));
+
+    }
+
+    public void exit(View view) {
+        finish();
     }
 }
