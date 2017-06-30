3d4eeea814f06c5786cc61767f8d9bef5d79140c
zhangbaoqi <764522533@qq.com>
2017-06-30 13:28:57 +0800

    引入ResultUtils.java

--- app/src/main/java/com/example/hzu/jkbd/utils/ResultUtils.java
+++ app/src/main/java/com/example/hzu/jkbd/utils/ResultUtils.java
@@ -1,8 +1,67 @@
 package com.example.hzu.jkbd.utils;
+        import android.util.Log;
 
-/**
- * Created by Administrator on 2017/6/30.
- */
+        import com.google.gson.Gson;
+
+        import org.json.JSONArray;
+        import org.json.JSONObject;
+
+        import java.util.ArrayList;
+        import java.util.List;
+
+        import com.example.hzu.jkbd.bean.Question;
+        import com.example.hzu.jkbd.bean.Result;
+        import com.example.hzu.jkbd.bean.ExamInfo;
 
 public class ResultUtils {
+    static String UTF_8 = "utf-8";
+    public static Result getListResultFromJson(String jsonStr){
+        Result result = new Result();
+        Log.e("Utils","jsonStr="+jsonStr);
+        try {
+            if(jsonStr==null || jsonStr.isEmpty() || jsonStr.length()<3)return null;
+            JSONObject jsonObject = new JSONObject(jsonStr);
+            if(!jsonObject.isNull("error_code")) {
+                result.setError_code(jsonObject.getInt("error_code"));
+            }else if(!jsonObject.isNull("msg")){
+                result.setError_code(jsonObject.getInt("msg"));
+            }
+            if(!jsonObject.isNull("reason")) {
+                result.setReason(jsonObject.getString("reason"));
+            }else if(!jsonObject.isNull("result")){
+                result.setReason(jsonObject.getString("result"));
+            }
+            if(!jsonObject.isNull("result")) {
+                JSONArray array = jsonObject.getJSONArray("result");
+                if (array != null) {
+                    List<Result.ResultBean> list = new ArrayList<Result.ResultBean>();
+                    for (int i = 0; i < array.length(); i++) {
+                        JSONObject jsonGroupAvatar = array.getJSONObject(i);
+                        Result.ResultBean ga = new Gson().fromJson(jsonGroupAvatar.toString(), Result.ResultBean.class);
+                        list.add(ga);
+                    }
+                    result.setResult(list);
+                    return result;
+                }
+            }else{
+                JSONArray array = new JSONArray(jsonStr);
+                if (array != null) {
+                    List<Result.ResultBean> list = new ArrayList<Result.ResultBean>();
+                    for (int i = 0; i < array.length(); i++) {
+                        JSONObject jsonGroupAvatar = array.getJSONObject(i);
+
+                        Result.ResultBean ga = new Gson().fromJson(jsonGroupAvatar.toString(),Result.ResultBean.class);
+                        list.add(ga);
+                    }
+                    result.setResult(list);
+                    return result;
+                }
+            }
+            return result;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return  null;
+    }
+
 }
